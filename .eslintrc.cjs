2ffce9d483fbb59484b6d684aa1f6ca7df3df9a1
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-03-19 16:16:12 +0100

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  overrides: [],
+  parserOptions: {
+    emaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/state-in-constructor": [enabled, mode],
+  },
+};

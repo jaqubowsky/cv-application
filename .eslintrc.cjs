afc180a1054eb36821be94fba3c8e6edf8532e2f
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-03-19 22:23:44 +0100

    Add Jest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "prettier",
+    "plugin:jest/recommended",
+  ],
   overrides: [],
   parserOptions: {
     emaFeatures: {
@@ -12,7 +17,7 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react"],
+  plugins: ["react", "jest"],
   rules: {
     "react/state-in-constructor": [enabled, mode],
   },

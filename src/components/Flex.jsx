b0151bec26ff927c874c9efa6ac9342cd915dca3
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-03-21 18:09:06 +0100

    Add props to be more flexible

--- src/components/Flex.jsx
+++ src/components/Flex.jsx
@@ -4,21 +4,42 @@ import { PropTypes } from "prop-types";
 
 const StyledFlex = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: ${(props) => props.direction};
   gap: ${(props) => props.gap};
+  padding: ${(props) => props.padding};
+  width: ${(props) => props.width};
+  background: ${(props) => props.background};
 `;
 
-function Flex({ children, gap }) {
-  return <StyledFlex gap={gap}>{children}</StyledFlex>;
+function Flex({ children, gap, padding, direction, width, background }) {
+  return (
+    <StyledFlex
+      gap={gap}
+      padding={padding}
+      width={width}
+      background={background}
+      direction={direction}
+    >
+      {children}
+    </StyledFlex>
+  );
 }
 
 Flex.propTypes = {
   children: PropTypes.node.isRequired,
   gap: PropTypes.string,
+  padding: PropTypes.string,
+  direction: PropTypes.string,
+  width: PropTypes.string,
+  background: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
 };
 
 Flex.defaultProps = {
   gap: "",
+  padding: "",
+  direction: "column",
+  width: "",
+  background: "",
 };
 
 export default Flex;

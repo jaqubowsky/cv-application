cd51e33ad4cbfc84b02cebadee51bf96d52b4fe8
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-03-22 14:30:02 +0100

    Clean code

--- src/pages/home/PersonalInfo.jsx
+++ src/pages/home/PersonalInfo.jsx
@@ -1,27 +1,45 @@
 import React, { PureComponent } from "react";
-import styled from "styled-components";
+import { PropTypes } from "prop-types";
+import uuid4 from "uuid4";
 import Input from "../../components/Input";
-
-const StyledPersonalInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1em;
-`;
+import Flex from "../../components/Flex";
 
 class PersonalInfo extends PureComponent {
   render() {
-    return (
-      <StyledPersonalInfo>
-        <h2>Personal Information</h2>
-        <Input type="text" name="firstName" placeholder="First Name" />
-        <Input type="text" name="lastName" placeholder="Last Name" />
-        <Input type="text" name="address" placeholder="Address" />
-        <Input type="number" name="phoneNumber" placeholder="Number" />
-        <Input type="text" name="email" placeholder="Email" />
-        <Input type="text" name="description" placeholder="Description" />
-      </StyledPersonalInfo>
-    );
+    const { handleChange, cvData, id } = this.props;
+
+    // Didn't have clue how to handle this otherwise and I wanted to use map function...
+    const placeholders = [
+      "First Name",
+      "Last Name",
+      "Address",
+      "Number",
+      "Email",
+      "Description",
+    ];
+
+    const inputElement = Object.keys(cvData).map((data, idx) => {
+      if (data === "id") return null;
+      return (
+        <Input
+          type={data === "phoneNumber" ? "number" : "text"}
+          name={data}
+          placeholder={placeholders[idx]}
+          value={[cvData[data]]}
+          key={uuid4()}
+          handleChange={(e) => handleChange(e, "personalInfo", id)}
+        />
+      );
+    });
+
+    return <Flex gap="1.2em">{inputElement}</Flex>;
   }
 }
+
+PersonalInfo.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  cvData: PropTypes.objectOf(PropTypes.string).isRequired,
+  id: PropTypes.string.isRequired,
+};
 
 export default PersonalInfo;
